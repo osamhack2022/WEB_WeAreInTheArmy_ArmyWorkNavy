21a0cfa53150efca7b945146ee9f8bec8466e3c6
MJ <80272444+minai621@users.noreply.github.com>
2022-09-29 10:15:49 +0000

    apply palette tailwind Theme

--- WEB_FE/React_WeAreInTheArmy/tailwind.config.cjs
+++ WEB_FE/React_WeAreInTheArmy/tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        "main": "#F7EFE8",
+        "seconddary": "#A42116",
+        "paper": "#F7F4F3",
+        "introduce": "#E3CDC5h"
+      },
       fontFamily: {
         NotoSansKR: ["Noto Sans KR", "cursive"]
       }
