e3d715238182b2ae4d7e216f85d9de090e8a4100
MJ <80272444+minai621@users.noreply.github.com>
2022-10-02 13:28:12 +0000

    fixing logim Template

--- WEB_FE/React_WeAreInTheArmy/src/App.tsx
+++ WEB_FE/React_WeAreInTheArmy/src/App.tsx
@@ -5,16 +5,16 @@ import LoginTemplate from './components/Templates/Login/LoginTemplate';
 import MainTemplate from './components/Templates/Main/MainTemplate';
 import SignupTemplate from './components/Templates/Signup/SignupTemplate';
 import FlexContainer from './components/UI/FlexContantainer';
-import { URL } from './data/url';
+import { Url } from './data/url';
 
 function App() {
   const location = useLocation();
-  const title = URL.find((v) => v.to === location.pathname);
+  const title = Url.find((v) => v.to === location.pathname);
   console.log(title);
   return (
-    <div className="App h-screen font-Noto Sans KR">
+    <div className="App h-full font-Noto Sans KR">
       <HeaderTemplate />
-      <FlexContainer className="items-center flex-col bg-main">
+      <FlexContainer className="items-center flex-col bg-main h-full">
         <FlexContainer className="min-w-[800px] w-full flex-col items-center">
           {title?.name !== '메인' && (
             <div className="w-[800px] text-slate-400 mt-[30px]">
