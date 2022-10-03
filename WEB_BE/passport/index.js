75efbd30c80ab80426bd0ca8d9be70cdf5c419f7
mireflare <mireflare@gmail.com>
2022-10-03 22:37:13 +0900

    register, login

--- WEB_BE/passport/index.js
+++ WEB_BE/passport/index.js
@@ -1,19 +1,19 @@
 const passport = require("passport");
 const local = require("./localStrategy");
-const kakao = require("./kakaoStrategy");
-const User = require("../models/user");
+// const kakao = require("./kakaoStrategy");
+const { userAccount: ua } = require("../models");
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user.identifier);
-    });
+	passport.serializeUser((user, done) => {
+		done(null, user.identifier);
+	});
 
-    passport.deserializeUser((id, done) => {
-        User.findOne({where: {id}})
-        .then(user => done(null, user))
-        .catch(err => done(err));
-    });
+	passport.deserializeUser((identifier, done) => {
+		ua.findOne({ where: { identifier } })
+			.then((user) => done(null, user))
+			.catch((err) => done(err));
+	});
 
-    local();
-    kakao();
-}
+	local();
+	// kakao();
+};
--- WEB_BE/routes/index.js
+++ WEB_BE/routes/index.js
@@ -1,22 +1,21 @@
 const express = require("express");
+
+const { isLoggedIn, isNotLoggedIn } = require("./loginStatus");
+
 const router = express.Router();
 
-const {isLoggedIn, isNotLoggedIn } = require("./loginStatus");
-
-router.get("/", function(req, res, next) {
-    res.render("index", {title: "Express"});
-    next();
+router.get("/", (req, res, next) => {
+	res.render("index", { title: "Express" });
+	next();
 });
 
-router.get("/loggedin", isLoggedIn, (req, res) => {
-    res.send("is Loggedin");
+router.get("/loggedin", isLoggedIn, (req, res, next) => {
+	res.send("is Loggedin");
+});
 
-})
+router.get("/notloggedin", isNotLoggedIn, (req, res, next) => {
+	res.send("is not Loggedin");
+});
 
-router.get("/notloggedin", isNotLoggedIn, (req, res) => {
-    res.send("is not Loggedin");
-})
-
-router.use("/userAccount", require("./uaSQL"));
 
 module.exports = router;
