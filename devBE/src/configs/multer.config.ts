d149f0073ca28dbb209b16f7216ab88efba7b8cb
mireflare <mireflare@gmail.com>
2022-10-17 03:07:15 +0900

    a file leaked

---
+++ devBE/src/configs/multer.config.ts
@@ -0,0 +1,37 @@
+import { NotAcceptableException } from "@nestjs/common";
+import { existsSync, mkdirSync } from "fs";
+import { diskStorage } from "multer";
+import { extname, join } from "path";
+import {v4 as uuid} from "uuid"
+
+export const multerConfigs = {
+    fileFilter: (request, file, callback) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png|heic)$/)) {
+            // allowed image formats
+            callback(null, true);
+        } else {
+            throw new NotAcceptableException("The format must be in jpg, jpeg, png, heic.")
+        }
+    },
+    storage: diskStorage({
+        destination: (request, file, callback) => {
+            const uploadPath = join(__dirname, "..", "..", "public", "reqImages");
+
+            if (!existsSync(uploadPath)) {
+                // Create uploadPath if not exists
+                mkdirSync(uploadPath, {recursive: true});
+            }
+            callback(null, uploadPath);
+        },
+
+        filename: (request, file, callback) => {
+            // file name when it is saved
+            callback(null, `${uuid()}${extname(file.originalname)}`);
+        }
+    })
+}
+
+export const createImageURL = (file): string => {
+    return `${__dirname}/../../public/reqImages/${file.filename}`
+}
+
