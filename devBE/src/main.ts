85b62ea0ff9ac40fd2dfcecd41f11382eef00d01
MJ <80272444+minai621@users.noreply.github.com>
2022-10-23 13:18:38 +0000

    be add cors

--- devBE/src/main.ts
+++ devBE/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { setUpSwagger } from './util/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  setUpSwagger(app);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors();
+	setUpSwagger(app);
+	await app.listen(3000);
 }
 bootstrap();
